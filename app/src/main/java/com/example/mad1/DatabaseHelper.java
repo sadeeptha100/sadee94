45c5073bb2df20406b14f93e9c7f57d6274de3dd
sadeeptha100 <sadeepthalakjaya@gmail.com>
2020-10-04 23:44:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad1/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.example.mad1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "Notes";
+
+    public DatabaseHelper( Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Notes TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int NewVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate (db);
+    }
+}
